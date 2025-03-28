4c4bd5b081f9295246b3e9deb21e763af142b892
Mursulov <maks10122003m@gmail.com>
2025-03-28 14:00:49 +0300

    Formatted code using clang-format (Mozilla style)

--- hello_word.cpp
+++ hello_word.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-//1
-int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello, world from " << name << "!";
-    return 0;
+// 1
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello, world from " << name << "!";
+  return 0;
 }
-
